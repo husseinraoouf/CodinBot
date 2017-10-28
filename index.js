9f7f50177934e22d35c7e0bacf3c7b48645e1332
El-Hussein Abdelraouf <husseinraoouf@gmail.com>
2017-10-28 12:20:43 +0200

    try 4

--- index.js
+++ index.js
@@ -127,6 +127,24 @@ function handleMessage(sender_psid, received_message) {
     callSendAPI(sender_psid, response);    
 }
 
+function handlePostback(sender_psid, received_postback) {
+    
+    let response;
+
+    // Get the payload for the postback
+    let payload = received_postback.payload;
+
+    // Set the response based on the postback payload
+    if (payload === 'yes') {
+        response = { "text": "Thanks!" }
+    } else if (payload === 'no') {
+        response = { "text": "Oops, try sending another image." }
+    }
+    
+    // Send the message to acknowledge the postback
+    callSendAPI(sender_psid, response);
+}
+
 
 function callSendAPI(sender_psid, response) {
     // Construct the message body
@@ -152,24 +170,6 @@ function callSendAPI(sender_psid, response) {
     }); 
 }
 
-
-function handlePostback(sender_psid, received_postback) {
-    
-    let response;
-
-    // Get the payload for the postback
-    let payload = received_postback.payload;
-
-    // Set the response based on the postback payload
-    if (payload === 'yes') {
-        response = { "text": "Thanks!" }
-    } else if (payload === 'no') {
-        response = { "text": "Oops, try sending another image." }
-    }
-    
-    // Send the message to acknowledge the postback
-    callSendAPI(sender_psid, response);
-}
 
 
 const PORT = process.argv[2] || 5000;
