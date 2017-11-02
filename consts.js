2ead7d426d2f0f81ecd19eb9ab80f09655e2076a
El-Hussein Abdelraouf <husseinraoouf@gmail.com>
2017-11-02 17:18:07 +0200

    fix3

--- consts.js
+++
@@ -1,9 +0,0 @@
-
-// module.exports.mongouri = "mongodb://localhost:27017/codinbot";
-module.exports.mongouri = "mongodb://hussein:123456as@ds243325.mlab.com:43325/heroku_s3jkv54n";
-
-
-module.exports.FB_PAGE_ACCESS_TOKEN = "EAAMCQLDgqWQBAOzEixL9Fbg95IzMRXjVsqTMJu0VyjCCHlsSAkBdml0sI6dMdu7sV5LjuAfYVKmc3YYZAdEXozgEaokFNTik40hKrK8g06sDqVrhPuulLt7ZCChKsGd7KCPy5W09cZAbj466ZCaPhuMbZBemd7ue57yDsunP9Hxj3nsVbgOtC";
-
-
-module.exports.APIAI_CLIENT  = "1f56152188f646b38ab963a7111d2168";
