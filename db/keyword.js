bdfdde26fbd5ebc1b90740e5f0699d76d8b8f7a2
El-Hussein Abdelraouf <husseinraoouf@gmail.com>
2017-11-03 22:37:33 +0200

    try

--- db/keyword.js
+++ db/keyword.js
@@ -3,8 +3,11 @@ module.exports = ({ Keywords, Users }) => {
     let methods = {}
 
     methods.addrating = async (lang, keyword, rate) => {
-        const response = await Keywords.update({ keyword: keyword, language: lang }, {$set: {rating: rate}});
+        console.log (lang, keyword, rate );
 
+        const response = await Keywords.update({ keyword: keyword, language: lang }, {$set: {rating: rate}, $inc: { numrated: 1}});
+
+        console.log(response);
     }
     return methods;
 }
