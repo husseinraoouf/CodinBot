557542729956af695331f6afa228f8cc1c491975
El-Hussein Abdelraouf <husseinraoouf@gmail.com>
2017-11-08 23:24:43 +0200

    fix12

--- index.js
+++ index.js
@@ -194,14 +194,14 @@ const start = async () => {
                     re = re.split(" ");
 
                     console.log(JSON.stringify(re));
-                    var ty = re[0]
+                    var ty = re[0];
                     for (var i = 1; i <= re.length; i++) {
                         console.log(re[i]);
                         if (re[i].length == 0) {
                             continue;
                         }
 
-                        if (i = re.length && ty.length > 0) {
+                        if (i == re.length && ty.length > 0) {
                             sendText(sender_psid, ty);                            
                         } else if (re[i].length + ty.length + 1 <= 640) {
                             ty += "\u000A" + re[i];
