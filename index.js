7b38d438ca472167aa11152e0b3b023404935814
El-Hussein Abdelraouf <husseinraoouf@gmail.com>
2017-11-11 15:32:02 +0200

    fix3

--- index.js
+++ index.js
@@ -567,52 +567,52 @@ const start = async () => {
                         "top_element_style": "compact",
                         "elements": [
                             {
-                            "title": "Classic T-Shirt Collection",
-                            "subtitle": "See all our colors",
-                            "image_url": "https://peterssendreceiveapp.ngrok.io/img/collection.png",          
-                            "buttons": [
-                                {
-                                "title": "View",
-                                "type": "web_url",
-                                "url": "https://peterssendreceiveapp.ngrok.io/collection",
-                                "messenger_extensions": true,
-                                "webview_height_ratio": "tall",
-                                "fallback_url": "https://peterssendreceiveapp.ngrok.io/"            
-                                }
-                            ]
+                                "title": "Classic T-Shirt Collection",
+                                "subtitle": "See all our colors",
+                                "image_url": "http://code.emc.com/images/code_icon.png",          
+                                "buttons": [
+                                    {
+                                        "title": "View",
+                                        "type": "web_url",
+                                        "url": "https://www.google.com.eg/",
+                                        "messenger_extensions": true,
+                                        "webview_height_ratio": "tall",
+                                        "fallback_url": "https://www.google.com.eg/"            
+                                    }
+                                ]
                             },
                             {
-                            "title": "Classic White T-Shirt",
-                            "subtitle": "See all our colors",
-                            "default_action": {
-                                "type": "web_url",
-                                "url": "https://peterssendreceiveapp.ngrok.io/view?item=100",
-                                "messenger_extensions": true,
-                                "webview_height_ratio": "tall",
-                                "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
-                            }
+                                "title": "Classic White T-Shirt",
+                                "subtitle": "See all our colors",
+                                "default_action": {
+                                    "type": "web_url",
+                                    "url": "https://www.google.com.eg/",
+                                    "messenger_extensions": true,
+                                    "webview_height_ratio": "tall",
+                                    "fallback_url": "https://www.google.com.eg/"
+                                }
                             },
                             {
-                            "title": "Classic Blue T-Shirt",
-                            "image_url": "https://peterssendreceiveapp.ngrok.io/img/blue-t-shirt.png",
-                            "subtitle": "100% Cotton, 200% Comfortable",
-                            "default_action": {
-                                "type": "web_url",
-                                "url": "https://peterssendreceiveapp.ngrok.io/view?item=101",
-                                "messenger_extensions": true,
-                                "webview_height_ratio": "tall",
-                                "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
-                            },
-                            "buttons": [
-                                {
-                                "title": "Shop Now",
-                                "type": "web_url",
-                                "url": "https://peterssendreceiveapp.ngrok.io/shop?item=101",
-                                "messenger_extensions": true,
-                                "webview_height_ratio": "tall",
-                                "fallback_url": "https://peterssendreceiveapp.ngrok.io/"            
-                                }
-                            ]        
+                                "title": "Classic Blue T-Shirt",
+                                "image_url": "http://code.emc.com/images/code_icon.png",
+                                "subtitle": "100% Cotton, 200% Comfortable",
+                                "default_action": {
+                                    "type": "web_url",
+                                    "url": "https://www.google.com.eg/",
+                                    "messenger_extensions": true,
+                                    "webview_height_ratio": "tall",
+                                    "fallback_url": "https://www.google.com.eg/"
+                                },
+                                "buttons": [
+                                    {
+                                        "title": "Shop Now",
+                                        "type": "web_url",
+                                        "url": "https://www.google.com.eg/",
+                                        "messenger_extensions": true,
+                                        "webview_height_ratio": "tall",
+                                        "fallback_url": "https://www.google.com.eg/"            
+                                    }
+                                ]        
                             }
                         ],
                         "buttons": [
@@ -627,7 +627,7 @@ const start = async () => {
             }
 
             await callSendMessageAPI(sender_psid, response); 
-            
+
             await typeOff(sender_psid);
 
         }  else if (payload.action == 'listExamples') {
